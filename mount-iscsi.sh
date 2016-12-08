c7c6913c9f4032f4044905dc7e42c889258093f4
keepwn <keepwn@gmail.com>
2016-12-08 11:45:32 +0800

    Add trap, do umount when get SIGTERM

--- mount-iscsi.sh
+++ mount-iscsi.sh
@@ -1,21 +1,30 @@
 #!/bin/bash
 
-# Mount iscsi-storage.
-echo "Restarting iSCSI"
-service open-iscsi restart
+mount() {
+    echo "Restarting iSCSI"
+    service open-iscsi restart
 
-echo "Discovering and mounting"
-iscsiadm -m discovery -t st -p $IP
-iscsiadm -m node --targetname "$TARGETNAME" --portal "$IP:$PORT" --login
-sleep 5
+    echo "Discovering and mounting"
+    iscsiadm -m discovery -t st -p $IP
+    iscsiadm -m node --targetname "$TARGETNAME" --portal "$IP:$PORT" --login
+    sleep 1
 
-echo "Mounting new iSCSI-device"
-mount /dev/disk/by-path/ip-$IP:$PORT-iscsi-$TARGETNAME-lun-0-part1 /mnt/storage
-echo "[enter key to exit]"
-read
+    echo "Mounting new iSCSI-device"
+    mount /dev/disk/by-path/ip-$IP:$PORT-iscsi-$TARGETNAME-lun-0-part1 /mnt/storage
+}
 
-# Unmount and log out.
-echo "Unmounting iSCSI-mount"
-umount /mnt/storage
-iscsiadm -m node --targetname "$TARGETNAME" --portal "$IP:$PORT" --logout
+umount() {
+    echo "Umounting iSCSI-mount"
+    umount /mnt/storage
+    iscsiadm -m node --targetname "$TARGETNAME" --portal "$IP:$PORT" --logout
+}
+
+trap "echo 'will stop';umount;exit" SIGHUP SIGINT SIGQUIT SIGTERM
+
+# Mount
+mount
+read -p "[enter key to exit]"
+
+# Umount
+umount
 echo "exited $0"
